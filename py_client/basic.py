437f5ccac6a1d304517a74b615920751cc611a76
Volodymyr262 <volodiahedz26@gmail.com>
2023-10-02 17:42:48 +0200

    project configuring

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,6 @@
 import requests
 
-endpoint = 'https://htttpbin.org'
+endpoint = 'http://localhost:8000/'
+
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+print(get_response.text)
