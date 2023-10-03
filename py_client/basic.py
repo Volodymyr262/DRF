e0a0b2027b71788fed4959a582b1691379ea872e
Volodymyr262 <volodiahedz26@gmail.com>
2023-10-03 20:26:01 +0200

    product api creating serializers

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = 'http://localhost:8000/api/'
 
-get_response = requests.get(endpoint, json={"product_id": 1})
+get_response = requests.post(endpoint, json={'title': 'helo world'})
 print(get_response.json())
