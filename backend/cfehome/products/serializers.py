0b24379ad5812744c70e872498de9c63a0c6ca91
Volodymyr262 <volodiahedz26@gmail.com>
2023-10-15 17:50:26 +0200

    urls , reverse 3:33:45

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail',
+                                               lookup_field='pk')
+    edit_url = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['url',
+                  'edit_url',
+                  'pk',
+                  'title',
+                  'content',
+                  'price']
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse('product-edit', kwargs={'pk': obj.pk}, request=request)
