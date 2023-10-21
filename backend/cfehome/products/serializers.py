17e8abdf0dbf50fe8156a9baf50c5af6974781ad
Volodymyr262 <volodiahedz26@gmail.com>
2023-10-21 21:59:27 +0200

    3:53:03 validating

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from .validators import validate_title_no_hello
 
 
 class ProductSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='product-detail',
                                                lookup_field='pk')
     edit_url = serializers.SerializerMethodField(read_only=True)
+    title = serializers.CharField(validators=[validate_title_no_hello])
 
     class Meta:
         model = Product
@@ -16,6 +18,21 @@ class ProductSerializer(serializers.ModelSerializer):
                   'title',
                   'content',
                   'price']
+
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__exact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f'{value} is already a product name.')
+    #     return value
+
+    # def create(self, validated_data):
+    #     email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     return obj
+    #
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
         request = self.context.get('request')
