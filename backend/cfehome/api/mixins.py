5106434faec361741b24c2ba5a1ef7b7f96bd383
Volodymyr262 <volodiahedz26@gmail.com>
2023-10-30 12:28:19 +0100

    4:12:42

--- backend/cfehome/api/mixins.py
+++ backend/cfehome/api/mixins.py
@@ -8,10 +8,13 @@ class StaffEditorPermissionMixin():
 
 class UserQuerysetMixin():
     user_field = 'user'
+    allow_staff_view = False
 
     def get_queryset(self, *args, **kwargs):
+        user = self.request.user
         lookup_data = {}
         lookup_data[self.user_field] = self.request.user
         qs = super().get_queryset(*args, **kwargs)
-
+        if user.is_staff:
+            return qs
         return qs.filter(**lookup_data)
--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
 from .validators import validate_title_no_hello
+from api.serializers import UserPublicSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    user = UserPublicSerializer(read_only=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail',
                                                lookup_field='pk')
     edit_url = serializers.SerializerMethodField(read_only=True)
@@ -19,7 +22,13 @@ class ProductSerializer(serializers.ModelSerializer):
                   'pk',
                   'title',
                   'content',
-                  'price']
+                  'price',
+                  'my_user_data']
+
+    def get_my_user_data(self, obj):
+        return {
+            'username': obj.user
+        }
 
     # def validate_title(self, value):
     #     qs = Product.objects.filter(title__exact=value)
